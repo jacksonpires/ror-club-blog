3473ef7442ccba6eeec90a2398208b0c18c0d672
Jackson Pires <jackson.pires@gmail.com>
2023-06-25 18:00:43 -0300

    Adiciona namespace administrate

--- app/controllers/administrate/posts_controller.rb
+++ app/controllers/administrate/posts_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class PostsController < ApplicationController
+class Administrate::PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   # GET /posts or /posts.json
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to(post_url(@post), notice: "Post was successfully created.") }
+        format.html { redirect_to(administrate_post_url(@post), notice: "Post was successfully created.") }
         format.json { render(:show, status: :created, location: @post) }
       else
         format.html { render(:new, status: :unprocessable_entity) }
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to(post_url(@post), notice: "Post was successfully updated.") }
+        format.html { redirect_to(administrate_post_url(@post), notice: "Post was successfully updated.") }
         format.json { render(:show, status: :ok, location: @post) }
       else
         format.html { render(:edit, status: :unprocessable_entity) }
@@ -53,7 +53,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to(posts_url, notice: "Post was successfully destroyed.") }
+      format.html { redirect_to(administrate_posts_path, notice: "Post was successfully destroyed.") }
       format.json { head(:no_content) }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts
+
+  namespace :administrate do
+    get "/", to: "posts#index"
+    resources :posts
+  end
+
+  get "/inicio", to: "welcome#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
