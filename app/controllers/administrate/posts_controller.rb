cb9b18ab039c937a9d756e871e220c1a41c5e020
Jackson Pires <jackson.pires@gmail.com>
2023-06-26 20:53:29 -0300

    Adiciona friendly id para posts

--- app/controllers/administrate/posts_controller.rb
+++ app/controllers/administrate/posts_controller.rb
@@ -61,7 +61,7 @@ class Administrate::PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find(params[:id])
+      @post = Post.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
