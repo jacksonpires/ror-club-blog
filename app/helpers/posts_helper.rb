b9cc0247ed910b4636ce4ed1fb3a74b474af5249
Jackson Pires <jackson.pires@gmail.com>
2023-07-12 21:58:46 -0300

    Criando um helper para fallback das imagens

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 module PostsHelper
+  def cover_image_fallback(post)
+    post.cover_image.attached? ? post.cover_image.variant(:thumb) : "posts/post1.jpg"
+  end
 end
