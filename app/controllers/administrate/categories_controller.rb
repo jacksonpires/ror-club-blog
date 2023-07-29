924ddfc44f1ab118008ffa7046c1b4b7ab6595f4
Jackson Pires <jackson.pires@gmail.com>
2023-07-29 02:25:53 -0300

    Adicionando o CRUD no admin para as categorias (new)

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -3,4 +3,28 @@ class Administrate::CategoriesController < AdministrateController
   def index
     @categories = Category.all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to(administrate_post_url(@category), notice: "Uma categoria foi criada com sucesso!") }
+        format.json { render(:show, status: :created, location: @category) }
+      else
+        format.html { render(:new, status: :unprocessable_entity) }
+        format.json { render(json: @category.errors, status: :unprocessable_entity) }
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
