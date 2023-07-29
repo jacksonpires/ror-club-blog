0b97f964ce766b19eecaa18b08487703ade279ea
Jackson Pires <jackson.pires@gmail.com>
2023-07-29 02:43:03 -0300

    Adicionando o CRUD no admin para as categorias (edit/show)

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 class Administrate::CategoriesController < AdministrateController
+  before_action :set_category, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @categories = Category.all
   end
@@ -8,12 +10,18 @@ class Administrate::CategoriesController < AdministrateController
     @category = Category.new
   end
 
+  def edit
+  end
+
+  def show
+  end
+
   def create
     @category = Category.new(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to(administrate_post_url(@category), notice: "Uma categoria foi criada com sucesso!") }
+        format.html { redirect_to(administrate_category_url(@category), notice: "Uma categoria foi criada com sucesso!") }
         format.json { render(:show, status: :created, location: @category) }
       else
         format.html { render(:new, status: :unprocessable_entity) }
@@ -22,7 +30,23 @@ class Administrate::CategoriesController < AdministrateController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @category.update(category_params)
+        format.html { redirect_to(administrate_category_url(@category), notice: "Uma categoria foi atualizada com sucesso!") }
+        format.json { render(:show, status: :ok, location: @category) }
+      else
+        format.html { render(:edit, status: :unprocessable_entity) }
+        format.json { render(json: @category.errors, status: :unprocessable_entity) }
+      end
+    end
+  end
+
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
