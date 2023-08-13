4ad2238c095b00cbcca3e563b21deac70ec66024
Jackson Pires <jackson.pires@gmail.com>
2023-08-13 20:39:57 -0300

    Ajustando o controller para registrar o usuário ao fazer um comentário

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body).merge({ user: current_user })
   end
 end
