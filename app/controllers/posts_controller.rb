52c579e4a7a582c7f7927ed58333e7c0bf8bba1e
Jackson Pires <jackson.pires@gmail.com>
2023-08-12 23:56:00 -0300

    Ordenando os posts conforme escolha

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,14 @@ class PostsController < ApplicationController
   def show
     @post = Post.friendly.find(params[:id])
     @other_posts = Post.all.take(3)
+    @comments = order_comments_by
+  end
+
+  private
+
+  def order_comments_by
+    return @post.comments.order(created_at: :asc) if params[:order_by] == "more_recents"
+
+    @post.comments.order(created_at: :desc)
   end
 end
