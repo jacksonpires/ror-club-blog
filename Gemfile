a7257828a659f9c20fd663954e562d3c65ab1aa3
Jackson Pires <jackson.pires@gmail.com>
2023-08-17 08:48:24 -0300

    Conhecendo e usando Markdown em nosso blog (parte 2)

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem "friendly_id", "~> 5.4.0"
 gem "devise", "~> 4.9"
 gem "image_processing", ">= 1.2"
 gem "pagy", "~> 6.0"
+gem "redcarpet"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,9 @@ module PostsHelper
   def cover_image_fallback(post, variant = :thumb)
     post.cover_image.attached? ? post.cover_image.variant(variant) : "posts/post1.jpg"
   end
+
+  def markdown(text)
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :fenced_code_blocks]
+    Markdown.new(text, *options).to_html.html_safe
+  end
 end
